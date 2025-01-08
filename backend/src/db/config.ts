47b3b35c7eeef848267027be32383d86bfa97098
Volodymyr Maliuchenko <mvv.feo@gmail.com>
2025-01-08 10:59:30 +0100

    Added default postgres port value

--- backend/src/db/config.ts
+++ backend/src/db/config.ts
@@ -3,7 +3,7 @@ import { DataSourceOptions } from 'typeorm';
 export const defaultDatabaseConfig: DataSourceOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
+  port: Number(process.env.POSTGRES_PORT) || 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
